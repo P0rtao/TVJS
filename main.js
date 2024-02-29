95ed53192f23246d2ff4623e193cbd5a04241b21
Portato <150350829+P0rtao@users.noreply.github.com>
2024-02-28 23:06:53 -0100

    Big Update
    Audio and Even More Code hooray

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ var B2 = document.getElementById("buttonTwo")
 var B3 = document.getElementById("buttonThree")
 var B4 = document.getElementById("buttonFour")
 
+var buttonDiv = document.getElementById("buttondiv")
+
 var TV = document.getElementById("Screen")
 
 var msg = document.getElementById("message")
 var div = document.getElementById("TextBox")
+
+// Temporary variable for Interval
+var interval = 0
+
 // Animations for the TextBox
 const divAnim = [
     { opacity: "0%" },
     { opacity: "100%" },
 ]
 
-const divTime = {
+const AnimTime = {
     duration: 1000,
 }
 
@@ -22,6 +28,46 @@ const divAnim2 = [
     { opacity: "0%" },
 ]
 
+// Animations for the buttons
+const buttonShow = [
+    { left: "-50%"},
+    { left: "0%"},
+]
+
+const buttonHide = [
+    { left: "0%"},
+    { left: "-50%"},
+]
+
+//--------------------------------Button Toggler---------------------------------------------
+function toggleButtons(state = "OFF") {
+    if (state == "OFF") {
+        B1.removeEventListener("click", Story1)
+    } else if (state == "ON") {
+        B1.addEventListener("click", Story1)
+    }
+}
+//--------------------------------------------------------------------------------------------
+
+function audioHandler(audioId = "", state = "OFF") {
+    var music = document.getElementById(audioId)
+    window.clearInterval(interval)
+
+    if (state == "OFF") {
+        window.interval = setInterval(() => {
+            music.volume -= 0.1
+            if (music.volume <= 0) {
+                window.clearInterval(interval)
+            }
+        }, 50)
+    } else if (state == "ON") {
+        music.currentTime = 0
+        setTimeout(() => {
+            music.volume = 1
+        music.play()
+        }, 10);
+    }
+}
 
 function changeImg(image = "") {
     TV.style.backgroundImage = "url(img/static.gif)"
@@ -41,10 +87,14 @@ function typeText(message = "Hi", speed = 50) {
 }
 
 function Story1() {
-    B1.removeEventListener("click", Story1)
+    toggleButtons("OFF")
+    audioHandler("music1", "ON")
+
+    buttonDiv.style.left = "-50%"
+    buttonDiv.animate(buttonHide, AnimTime)
 
     div.style.opacity = "100%"
-    div.animate(divAnim, divTime).onfinish = function () {
+    div.animate(divAnim, AnimTime).onfinish = function () {
         let time = 6000
         
         setTimeout(typeText, 0 * time, "Era uma vez um Patinho.")
@@ -67,16 +117,21 @@ function Story1() {
 
         // Controls the reset after it ends
         setTimeout(() => {
+            
             div.style.opacity = "0%"
-            div.animate(divAnim2, divTime).onfinish = () => {
+            audioHandler("music1", "OFF")
+            
+            div.animate(divAnim2, AnimTime).onfinish = () => {
                 msg.innerHTML = ""
                 changeImg("static.gif")
+                buttonDiv.style.left = "0%"
+                buttonDiv.animate(buttonShow, AnimTime)
 
-                B1.addEventListener("click", Story1)
+                toggleButtons("ON")
             }
         }, 8 * time)
     }
 
 }
 
-B1.addEventListener("click", Story1)
+toggleButtons("ON")
